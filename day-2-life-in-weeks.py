f963d0f48cf4a75a9095325fd80de455b5b84932
Enéias Brum <57417495+eneiasbrumjr@users.noreply.github.com>
2023-08-05 09:48:38 -0300

    Create day-2-life-in-weeks.py

---
+++ day-2-life-in-weeks.py
@@ -0,0 +1,5 @@
+# 🚨 Don't change the code below 👇
+age = int(input("What is your current age? "))
+# 🚨 Don't change the code above 👆
+#Write your code below this line 👇
+print(f"You have {(90-age)*365} days, {(90-age)*52} weeks, and {(90-age)*12} months left.")
